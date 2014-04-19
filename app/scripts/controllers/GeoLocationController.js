3a4e2afe61677cbdd65ba38603a4b9b11083a9f4
raduj.lukasz@gmail.com <raduj.lukasz@gmail.com>
2014-04-19 21:30:57 +0200

    Add extensions to gitignore and delete unnecessary files

--- app/scripts/controllers/GeoLocationController.js
+++ app/scripts/controllers/GeoLocationController.js
@@ -2,22 +2,10 @@
 
 rsControllers.controller('GeoLocationController', ['$scope', '$window', function($scope, $window) {
 
-    $scope.geo = {
-        x: 31,
-        y: 4
-    };
-    
     $scope.printer = function(position) {
         $scope.$apply(function() {
             $scope.coords = position.coords
         });
-//        console.log("pos"  + position.coords.longitude);
-//        $scope.geo.x = 321;
-//        $scope.geo.latitude = position.coords.latitude;
-//        $scope.geo.longitude = position.coords.longitude
-//        };
-
-//        console.log($scope.geo);
     };
 
     var getLocation = function() {
